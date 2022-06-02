4241445000e90da449c8ce12c291757d3a6c34ab
jihun1233 <leejihun1233@gmail.com>
2022-06-03 00:14:10 +0900

    first commit
    - cra + scss module + favicon + typescript + eslint + prettier

---
+++ src/hooks/index.tsx
@@ -0,0 +1,5 @@
+export { useClickAway, useInterval, usePrevious, useMount, useUnmount } from 'react-use'
+export { useState, useEffect, useLayoutEffect, useCallback, useContext, useMemo, useReducer, useRef } from 'react'
+
+export { useAppDispatch } from './useAppDispatch'
+export { useAppSelector } from './useAppSelector'
---
+++ src/routes/index.jsx
@@ -0,0 +1,7 @@
+import styles from './routes.module.scss'
+
+const App = () => {
+  return <div className={styles.app}>Hello</div>
+}
+
+export default App
