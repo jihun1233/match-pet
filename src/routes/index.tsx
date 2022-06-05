e227a7f7b7c2fdcbfdb31ee65c1b302d3c6b007c
jihun1233 <leejihun1233@gmail.com>
2022-06-05 13:52:35 +0900

    라우팅 element 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Match from './Match'
+import MatchList from './MatchList'
+import RejectList from './RejectList'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='' element={<Match />} />
-        <Route path='match-list' />
-        <Route path='reject-list' />
+        <Route path='match-list' element={<MatchList />} />
+        <Route path='reject-list' element={<RejectList />} />
         <Route path='*' element={<div>not found!</div>} />
       </Route>
     </Routes>
