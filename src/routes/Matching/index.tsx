0945980f3204bad53978af86c1a79033953627f5
jihun1233 <leejihun1233@gmail.com>
2022-06-05 14:04:30 +0900

    Rename: Match -> Matching으로 이름 변경

--- src/routes/Matching/index.tsx
+++ src/routes/Matching/index.tsx
@@ -1,21 +1,17 @@
-import { useEffect } from 'react'
-import { getUsers, getMatches, getRejects, setUsers, getPage, increasePage } from 'states/match'
+import { getUsers, setUsers, getPage, increasePage } from 'states/match'
 import { useAppDispatch, useAppSelector } from 'hooks'
-import store from 'store'
 
 import styles from './match.module.scss'
 import ProfileCard from './ProfileCard'
 import { getUsersApi } from 'services/match'
 import { useQuery } from 'react-query'
 
-const Match = () => {
+const Matching = () => {
   const dispatch = useAppDispatch()
   const users = useAppSelector(getUsers)
-  const matches = useAppSelector(getMatches)
-  const rejects = useAppSelector(getRejects)
   const page = useAppSelector(getPage)
 
-  const { data, isLoading } = useQuery('getUsers', () => getUsersApi(page), {
+  useQuery('getUsers', () => getUsersApi(page), {
     enabled: users.length === 0,
     onSuccess: (res) => {
       dispatch(setUsers(res))
@@ -38,4 +34,4 @@ const Match = () => {
   )
 }
 
-export default Match
+export default Matching
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
-import Match from './Match'
+import Matching from './Matching'
 import MatchList from './MatchList'
 import RejectList from './RejectList'
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route path='' element={<Match />} />
+        <Route path='' element={<Matching />} />
         <Route path='match-list' element={<MatchList />} />
         <Route path='reject-list' element={<RejectList />} />
         <Route path='*' element={<div>not found!</div>} />
