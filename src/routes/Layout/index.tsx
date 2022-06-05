541a3449ba09a8a806cc224b590f3604230bd35f
jihun1233 <leejihun1233@gmail.com>
2022-06-05 10:13:45 +0900

    Add: GNB 추가
    - SD사이즈 (768px) 미만일때는 하단 네비게이션으로, 이상일때는 좌측 사이드바로 사용할 수 있도록 함
    
    - NavLink를 사용해 active상태일때 메인테마색으로 변함

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import GNB from './GNB'
 import styles from './layout.module.scss'
 
 const Layout = () => {
   return (
     <div className={styles.layout}>
       <Outlet />
+      <GNB />
     </div>
   )
 }
