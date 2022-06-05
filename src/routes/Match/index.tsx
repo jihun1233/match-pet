d2a6298fecf9884c7d3caee4800039d11663335e
jihun1233 <leejihun1233@gmail.com>
2022-06-05 12:30:25 +0900

    Update: getUsersApi호출할 때 useQuery 사용
    - users.length가 0이 되면 useQuery를 통해 getUsersApi를 호출하고, 이 때 store와 redux로 관리되는 page를 함께 넘겨주어 해당하는 페이지의 이미지를 가져온다

--- src/routes/Match/index.tsx
+++ src/routes/Match/index.tsx
@@ -1,24 +1,27 @@
 import { useEffect } from 'react'
-import { getUsers, getMatches, getRejects, setUsers } from 'states/match'
+import { getUsers, getMatches, getRejects, setUsers, getPage, increasePage } from 'states/match'
 import { useAppDispatch, useAppSelector } from 'hooks'
 import store from 'store'
 
 import styles from './match.module.scss'
 import ProfileCard from './ProfileCard'
 import { getUsersApi } from 'services/match'
+import { useQuery } from 'react-query'
 
 const Match = () => {
   const dispatch = useAppDispatch()
   const users = useAppSelector(getUsers)
   const matches = useAppSelector(getMatches)
   const rejects = useAppSelector(getRejects)
+  const page = useAppSelector(getPage)
 
-  useEffect(() => {
-    if (store.get('match-pet-users')) return
-    getUsersApi().then((res) => {
+  const { data, isLoading } = useQuery('getUsers', () => getUsersApi(page), {
+    enabled: users.length === 0,
+    onSuccess: (res) => {
       dispatch(setUsers(res))
-    })
-  }, [dispatch])
+      dispatch(increasePage())
+    },
+  })
 
   if (users.length === 0) return null
 
@@ -26,7 +29,7 @@ const Match = () => {
     <div className={styles.match}>
       <ul>
         {users.map((user) => (
-          <li key={`user-list-${user.id}`}>
+          <li className={styles.listItem} key={`user-list-${user.id}`}>
             <ProfileCard user={user} />
           </li>
         ))}
