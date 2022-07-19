e8a992c601871f948cb1f61bb3f48ee4055c12b0
jihun1233 <leejihun1233@gmail.com>
2022-07-19 19:57:20 +0900

    Update: alarm icon 추가
    - alarm icon을 추가하여 클릭 시 메시지모달을 open

--- src/routes/Layout/Header/index.tsx
+++ src/routes/Layout/Header/index.tsx
@@ -1,10 +1,26 @@
 import styles from './header.module.scss'
 import { FaPaw } from 'react-icons/fa'
+import { AiOutlineBell } from 'react-icons/ai'
+import { cx } from 'styles'
+import { MouseEventHandler } from 'react'
+import { useAppDispatch } from 'hooks'
+import { openMessageModal } from 'states/modal'
 
 const Header = () => {
+  const dispatch = useAppDispatch()
+  const onAlarmIconClick: MouseEventHandler<SVGElement> = () => {
+    dispatch(openMessageModal())
+  }
+
   return (
     <header>
+      <div className={styles.left} />
       <FaPaw className={styles.logo} size='40px' />
+      <div className={styles.right}>
+        <div className={cx(styles.iconWrapper, { [styles.newMessage]: true })}>
+          <AiOutlineBell className={styles.alarmIcon} size={25} onClick={onAlarmIconClick} />
+        </div>
+      </div>
     </header>
   )
 }
