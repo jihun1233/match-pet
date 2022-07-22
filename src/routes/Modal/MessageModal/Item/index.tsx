946186ad473317959102dc506a9a6b38467e8880
jihun1233 <leejihun1233@gmail.com>
2022-07-22 21:09:58 +0900

    Update: message 읽음 상태 표시
    - 읽지 않은 메시지의 경우 확인 버튼으로 읽음상태 변환 가능
    
    - 읽은 메시지는 읽음으로 상태 표시

--- src/routes/Modal/MessageModal/Item/index.tsx
+++ src/routes/Modal/MessageModal/Item/index.tsx
@@ -1,13 +1,29 @@
+import { useAppDispatch } from 'hooks'
+import { readMessage } from 'states/modal'
+import { IMessage } from 'types/modal'
 import styles from './item.module.scss'
 
 interface IProps {
-  message: string
+  message: IMessage
 }
 
 const Item = ({ message }: IProps) => {
+  const dispatch = useAppDispatch()
+
+  const onReadButtonClick = () => {
+    dispatch(readMessage(message.id))
+  }
+
   return (
-    <div className={styles.messageModalItem}>
-      <p>{message}</p>
+    <div className={styles.message}>
+      <p className={styles.text}>{message.message}</p>
+      {message.hasRead ? (
+        <span className={styles.hasRead}>읽음</span>
+      ) : (
+        <button type='button' className={styles.readButton} onClick={onReadButtonClick}>
+          확인
+        </button>
+      )}
     </div>
   )
 }
